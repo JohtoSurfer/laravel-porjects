de9e71806252f8ae69f63b9b0e058b00e537c6e8
Luis Barrios <104099952+JohtoSurfer@users.noreply.github.com>
2024-10-14 14:22:21 -0400

    type:update. summary: small changes.

--- student-management-app/routes/web.php
+++ student-management-app/routes/web.php
@@ -1,6 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+/*
+web routes
+
+=== core section ===
+/dashboard
+/my profile
+/attendance 
+/subjects/classes
+/exams
+/progress
+/fees and payments
+/progress
+/noticeboard
+/settings
+/message
+*/ 
 
 Route::get('/', function () {
     return view('welcome');
